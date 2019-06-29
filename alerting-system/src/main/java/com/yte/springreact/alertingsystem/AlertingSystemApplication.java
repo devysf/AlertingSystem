a1d58b8732636e47b7564bebd21bfc88632b1519
devysf <ozacetyusuf@gmail.com>
2019-06-29 12:20:57 +0300

    Business logic is set with ScheduledExecutorService and Threads.

--- alerting-system/src/main/java/com/yte/springreact/alertingsystem/AlertingSystemApplication.java
+++ alerting-system/src/main/java/com/yte/springreact/alertingsystem/AlertingSystemApplication.java
@@ -1,15 +1,49 @@
 package com.yte.springreact.alertingsystem;
 
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import com.yte.springreact.alertingsystem.entity.Alerts;
+import com.yte.springreact.alertingsystem.service.AlertsService;
+
 
 @SpringBootApplication
 public class AlertingSystemApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AlertingSystemApplication.class, args);
+	
+	private static AlertsService alertsService;
+	
+	@Autowired
+	public AlertingSystemApplication(AlertsService theAlertsService) {
+		alertsService = theAlertsService;
 	}
+	
+	public static void main(String[] args) throws Exception{
+		SpringApplication.run(AlertingSystemApplication.class, args);
+		
+		
+		System.out.println("in main ");
+		
+		System.out.println(alertsService.findAll());
+		
+		List<Alerts> listOfAlerts = alertsService.findAll();
+		
+		for(Alerts alerts : listOfAlerts) {
+			MyThread th = new MyThread(alertsService,alerts);
+			
+			ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();	    
+			
+			service.scheduleAtFixedRate(th, 0, alerts.getPeriod(), TimeUnit.MILLISECONDS);
+			
+		}
+	
+	}
+	
+	
 
 }
---
+++ alerting-system/src/main/java/com/yte/springreact/alertingsystem/MyThread.java
@@ -0,0 +1,101 @@
+package com.yte.springreact.alertingsystem;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.yte.springreact.alertingsystem.entity.Alerts;
+import com.yte.springreact.alertingsystem.service.AlertsService;
+
+class MyThread implements Runnable {
+	Alerts alerts;
+	String name;
+	Thread t;
+		
+	private static AlertsService alertsService;
+	
+	@Autowired
+	public MyThread(AlertsService theAlertsService, Alerts theAlerts) {
+		alertsService = theAlertsService;
+		
+		alerts = theAlerts;
+		name = alerts.getName();
+		
+		t = new Thread(this, name);
+		System.out.println("New thread: " + t);
+	}
+	
+	
+	public void run() {
+		System.out.println("\n" + name + " entering.");
+
+		try{
+			 getStatus(alerts.getUrl(),alerts.getHttp_method());
+		}
+		catch (Exception e) {
+		     System.out.println(name + "Exception");
+		}
+		 
+		System.out.println(name + " exiting.\n");
+		
+	}
+	
+	public String getStatus(String url,String http_method) throws IOException {
+		 
+		String result = "";
+		int code = 200;
+		try {
+			URL siteURL = new URL(url);
+			HttpURLConnection connection = (HttpURLConnection) siteURL.openConnection();
+			connection.setRequestMethod(http_method);
+			connection.setConnectTimeout(1000);
+			connection.connect();
+ 
+			code = connection.getResponseCode();
+			if (code == 200) {
+				result = code  + " OK ";
+				
+				String alertsResult = alerts.getResult();
+				
+				alertsResult = alertsResult.substring(1, 6);
+					
+				alertsResult+="1"; 
+
+				alerts.setResult(alertsResult);
+				
+				alertsService.save(alerts);
+				
+				System.out.println("Alerts result : "+ alertsResult);
+
+				
+				
+			} else {
+				result = " " +  code;
+				
+				String alertsResult = alerts.getResult();
+				
+				alertsResult = alertsResult.substring(1, 6);
+				
+				alertsResult+="0" ;
+				
+				alerts.setResult(alertsResult);
+				
+				alertsService.save(alerts);
+				
+				System.out.println("Alerts result : "+ alertsResult);
+
+
+				
+			}
+		} catch (Exception e) {
+			result = "Wrong domain - Exception: " + e.getMessage();
+			
+			
+ 
+		}
+		System.out.println(url + "\t\tStatus Code: " + result);
+		return result;
+	}
+}
